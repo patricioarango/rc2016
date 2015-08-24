db369647383d253698a7b84572074527590c0d1f
patricio.arango <patricioarango@gmail.com>
2015-08-23 22:41:15 -0300

    limpieza

--- www/js/index.js
+++ www/js/index.js
@@ -52,10 +52,11 @@ onNotificationGCM: function(e) {
                 if ( e.regid.length > 0 )
                 {
                     var url = 'http://autowikipedia.es/phonegap/racing_calendar_insert_registerid.php?register_id=' + e.regid;
-                    $("#eventos").text(url);
+                    $("#eventos").append(url);
                     $.post(url, function(data) {
-                        $("#eventos").text("el id insertado es " + data);
+                            $("#eventos").append("<br>el id insertado es " + data);
                     });
+
                     //hago un post, mando el token, recibo el id de mi base y lo guardo con localstorage. Pongo una variable registrado 1 para no volver a hacer el proceso.
                     console.log("Regid " + e.regid);
                     $("#eventos").text("el e.regid es " + e.regid);
